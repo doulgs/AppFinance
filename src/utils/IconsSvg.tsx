23ebdbc0e6da6e608fc4eab642d1b6ceff36e908
Douglas Domiciano <54917227+doulgs@users.noreply.github.com>
2023-03-25 12:11:10 -0300

    Ícones

---
+++ src/utils/IconsSvg.tsx
@@ -0,0 +1,19 @@
+import HomeIcon from "../assets/home.svg";
+import HomeFillIcon from "../assets/homeFill.svg";
+import GraphicIcon from "../assets/graphic.svg";
+import GraphicFillIcon from "../assets/graphicFill.svg";
+import NotifiIcon from "../assets/notifications.svg";
+import NotifiFillIcon from "../assets/notificationsFill.svg";
+import SettingsIcon from "../assets/setting.svg";
+import SettingsFillIcon from "../assets/settingFill.svg";
+
+export {
+  HomeIcon,
+  HomeFillIcon,
+  GraphicIcon,
+  GraphicFillIcon,
+  NotifiIcon,
+  NotifiFillIcon,
+  SettingsIcon,
+  SettingsFillIcon,
+};
